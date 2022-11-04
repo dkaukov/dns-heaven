3d878ede0ef80476c4ff7d8fdb3c3666d339149e
Dmitry Kaukov <dkaukov@opentable.com>
2022-11-04 17:42:23 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 TARGET=/usr/local/bin/dns-heaven
 PLIST=/Library/LaunchDaemons/com.greenboxal.dnsheaven.plist
 
-curl -L -o $TARGET https://github.com/greenboxal/dns-heaven/releases/download/v1.0.0/dns-heaven_1.0.0_darwin_amd64
+curl -L -o $TARGET https://github.com/dkaukov/dns-heaven/releases/download/v1.1/dns-heaven_1.1_darwin_amd64
 chmod +x $TARGET
 
 cat > $PLIST <<EOF
